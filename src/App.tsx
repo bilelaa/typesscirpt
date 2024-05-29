8f0a7994564db045141d818fdeee3f515b09baff
bilelaa <biilelbenhlel@icloud.com>
2024-05-29 12:50:20 +0200

    checkpoint typescirpt

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Counter from './Counter'
+import Greeting from './Greetin'
+
+function App() {
+  
+
+  return (
+    <>
+     <Greeting name =""/>
+     <Counter/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Greetin.tsx
@@ -0,0 +1,7 @@
+
+
+const Greeting = ({name}:{name: string})=>{
+    return  <> hello {name}</>  
+
+}
+export default Greeting
